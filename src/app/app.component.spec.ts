733b18d17863cba042a6018d4d240de1b7eda1ae
Diones Rossetto <diones.francisco@totvs.com.br>
2021-10-15 22:14:42 -0300

    dev

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,9 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, waitForAsync } from '@angular/core/testing';
-
 import { RouterTestingModule } from '@angular/router/testing';
-
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-
 
   beforeEach(waitForAsync(() => {
 
@@ -23,16 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it('should have menu labels', waitForAsync(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const app = fixture.nativeElement;
-    const menuItems = app.querySelectorAll('ion-label');
-    expect(menuItems.length).toEqual(12);
-    expect(menuItems[0].textContent).toContain('Inbox');
-    expect(menuItems[1].textContent).toContain('Outbox');
-  }));
-
   it('should have urls', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
@@ -42,5 +29,4 @@ describe('AppComponent', () => {
     expect(menuItems[0].getAttribute('ng-reflect-router-link')).toEqual('/folder/Inbox');
     expect(menuItems[1].getAttribute('ng-reflect-router-link')).toEqual('/folder/Outbox');
   }));
-
 });
