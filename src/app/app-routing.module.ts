1c473c28d683ca5c85557f9b11f86530acb25ec3
Diones Rossetto <diones.francisco@totvs.com.br>
2021-10-19 21:16:05 -0300

    fim

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes,  } from '@angular/router';
 
-const routes: Routes = [
-  {
+const routes: Routes = [{
     path: '',
     redirectTo: 'prato/dia',
     pathMatch: 'full'
